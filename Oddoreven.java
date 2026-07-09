04e1da5ca0273cbce4774a02ab21136320b2cee8
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-09 21:41:16 +0530

    Add files via upload

---
+++ Oddoreven.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Oddoreven {
+
+	public static void main(String[] args) {
+	}{
+		int i = 7;
+		
+		if (i % 2 == 0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+}
+	
+
