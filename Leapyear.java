77917be19f1619e5b340acadfb585d125cb12838
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-19 01:20:03 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,17 @@
+package day1;
+import java.util.Scanner;
+public class Leapyear {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt() ;
+		
+		 if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+	            System.out.println(year + " is a Leap Year.");
+	        } else {
+	            System.out.println(year + " is Not a Leap Year.");
+	        }
+
+	}
+
+}
