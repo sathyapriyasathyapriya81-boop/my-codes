3552fece4fdb01a14f230769d9210b02cdf2d8e0
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-19 00:58:01 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Evenorodd {
+
+	public static void main(String[] args) {
+		int i = 0 ;
+		if (i /2 == 0) {System.out.println("even");		
+		}else {
+			System.out.println("odd");
+		}
+
+	}
+
+}
