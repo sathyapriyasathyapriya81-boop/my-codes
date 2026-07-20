96a14403079dfdb9aab27956c211fb501a35b13c
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-20 23:42:46 +0530

    Add files via upload

---
+++ Sumoftwodigits.java
@@ -0,0 +1,16 @@
+package day2;
+import java.util.Scanner;
+public class Sumoftwodigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number1 :");
+int a = sc.nextInt();
+System.out.println("Enter the number 2");
+int b = sc.nextInt();
+int c = a+b ;
+System.out.println("the total value is" + c);
+	}
+
+}
