845a4e42db8b14469cc66ece8878048e9690aeba
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-20 22:34:10 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+package day2;
+import java.util.Scanner;
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number");
+		int num = sc.nextInt();
+		int square = num*num;
+		System.out.println(square);
+	}
+
+}
