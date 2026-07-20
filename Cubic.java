ef34eecf4a35cabe9e656f8b0a00e761cee73584
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-20 22:35:31 +0530

    Add files via upload

---
+++ Cubic.java
@@ -0,0 +1,14 @@
+package day2;
+import java.util.Scanner;
+public class Cubic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number:");
+		int num = sc.nextInt();
+		int cubic = num*num*num ;
+		System.out.println(cubic);
+	}
+
+}
