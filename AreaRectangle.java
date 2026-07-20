8088528bb8571bc00e7014ddb198d643b40f291d
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-20 22:59:55 +0530

    Add files via upload

---
+++ AreaRectangle.java
@@ -0,0 +1,16 @@
+package day2;
+import java.util.Scanner;
+public class AreaRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the  length :");
+double length = sc.nextDouble();
+System.out.print("Enter the breath:");
+double breath = sc.nextDouble();
+double area = length *breath ;
+System.out.println("the are of rectangle :"+ area);
+	}
+
+}
