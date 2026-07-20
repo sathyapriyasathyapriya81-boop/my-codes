7062833a433fbafd4a490c69dc14675c84a056be
SATHIYAPRIYA <sathyapriyasathyapriya81@gmail.com>
2026-07-20 23:05:42 +0530

    Add files via upload

---
+++ Areaoftriangle.java
@@ -0,0 +1,15 @@
+package day2;
+import java.util.Scanner;
+public class Areaoftriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the breath :");
+double breath = sc.nextDouble();
+System.out.print("Enter the height :");
+double height =sc.nextDouble();
+double area = 0.5 *breath*height;
+System.out.println("the area is "+ area);}
+
+}
